8bb7d2655f5d4b0da3e57767028be367aa7f1220
lab008007 <lab008007@gmail.com>
2024-08-19 18:12:26 +0900

    ?????

---
+++ java/src/Day09/Ex07_Review/추상클래스/MacBook.java
@@ -0,0 +1,11 @@
+package Day09.Ex07_Review.추상클래스;
+
+public class MacBook extends Laptop {
+
+	@Override
+	public void typing() {
+		// TODO Auto-generated method stub
+		System.out.println("MacBook - typing");
+	}
+
+}
